e40f89ba36224e5ecae9361fdb993c83ad98235f
Can Çilek <46296419+CanCilek@users.noreply.github.com>
2023-10-04 19:02:46 +0300

    add job

--- Demo_Product/Controllers/JobController.cs
+++ Demo_Product/Controllers/JobController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,50 @@ namespace Demo_Product.Controllers
 {
     public class JobController : Controller
     {
+
+        JobManager jobManager = new JobManager(new EfJobDAL());
+
+        // GET: /<controller>/
         public IActionResult Index()
         {
+            var values = jobManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddJob()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddJob(Job p)
+        {
+      
+                jobManager.TInsert(p);
+                return RedirectToAction("Index");
+            
+  
+
+        }
+
+        public IActionResult DeleteJob(int id)
+        {
+            var value = jobManager.TGetById(id);
+            jobManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateJob(int id)
+        {
+            var value = jobManager.TGetById(id);
+
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateJob(Job p)
+        {
+
+            jobManager.TUpdate(p);
+            return RedirectToAction("Index");
         }
     }
 }
